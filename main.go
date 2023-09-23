ed3a07343f6ae808c820e76c269b9c7b398ffcf5
Pranoy Kundu <pranoy1998k@gmail.com>
2023-09-23 18:46:00 +0530

    use sync.OnceValue function

--- main.go
+++ main.go
@@ -5,13 +5,40 @@ import (
 	"sync"
 )
 
-func helloWorld() {
+type BookKeeper struct {
+	name        string
+	id          string
+	phoneNumber string
+}
+
+func NewBookKeeper(name, id, phoneNumber string) *BookKeeper {
+	return &BookKeeper{
+		name:        name,
+		id:          id,
+		phoneNumber: phoneNumber,
+	}
+}
+
+func (bk *BookKeeper) helloWorld() {
 	fmt.Println("Hello World!")
 }
 
+func (bk *BookKeeper) getName() string {
+	return bk.name
+}
+
 func main() {
-	once := sync.OnceFunc(helloWorld)
+	bk :=  NewBookKeeper("John", "123456789", "1234567890")
+
+	// sync.OnceFunc
+	once := sync.OnceFunc(bk.helloWorld)
 	for i := 0; i < 5; i++ {
 		once()
+	}
+
+	// sync.OnceValue
+	onceValue := sync.OnceValue[string](bk.getName)
+	for i := 0; i < 5; i++ {
+		fmt.Println(onceValue())
 	}
 }
